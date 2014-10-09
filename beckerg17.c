eb7605f89eccfe0db7f9387b3fa3d6af1924ef1b
garrett becker <beckerg17@up.edu>
2014-10-09 00:36:27 +0000

    created username function for beckerg17 favorite food

---
+++ beckerg17.c
@@ -0,0 +1,10 @@
+/* this file is beckerg17.c*/
+#include <stdio.h>
+#include "username.h"
+
+void beckerg17(){
+
+print ("beckerg17 like pizza\n");
+
+
+}
---
+++ beckerg17.h
@@ -0,0 +1,2 @@
+/* this is file name beckerg17.h*/
+extern void beckerg17();
